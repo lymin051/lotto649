f1fcf3597608589e073263d647506beeeb78e2e4
lazymanqk <65388041+lazymanqk@users.noreply.github.com>
2020-08-07 17:14:42 +0800

    Create Ex01.java

---
+++ Ex01.java
@@ -0,0 +1,15 @@
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		final float PI = 3.1415F;
+		float r1 = 10, r2 = 20, r3 = 50;
+		System.out.println("¥b®|:" + r1 + " ¶ê©Pªø:" + (r1 * 2) * PI + "  ­±¿n:" + (r1 * r1 * PI));
+		System.out.println("¥b®|:" + r2 + " ¶ê©Pªø:" + (r2 * 2) * PI + " ­±¿n:" + (r2 * r2 * PI));
+		System.out.println("¥b®|:" + r3 + " ¶ê©Pªø:" + (r3 * 2) * PI + " ­±¿n:" + (r3 * r3 * PI));
+		
+		
+
+	}
+
+}
